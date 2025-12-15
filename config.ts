883b540ef0a6507999d86f671541f0a0508fc3c8
lasjunior99 <lasjunior99@gmail.com>
2025-12-15 19:48:33 -0300

    ajuste de apps sheets

--- config.ts
+++ config.ts
@@ -3,6 +3,6 @@
 // ==========================================================
 
 // URL Fixa do Backend Google Apps Script
-export const API_URL = "https://script.google.com/macros/s/AKfycbwcvMMkW3yQ5z8Cmfxt5rAFQPzZsL_7zrSUnQQb_iqQqaZcawgVW0MkPz_ry-DuC4I2SQ/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbyDyLF3WJjJAZFSavPA9L8UvpRlkM7O7puovB0NRE5R/devhttps://script.google.com/macros/s/AKfycbzOvs602V0Epo12rotfqRu1kaMiwfFxPBu-RuemHD_IkapWRVTRbrqN-z058ZB0RZQS5Q/exec";
 
 export const SYSTEM_VERSION = "2.0 Distributed";
