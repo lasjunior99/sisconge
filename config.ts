8c6e91aec3e9aed8eaad2efed32fa0aece534472
lasjunior99 <lasjunior99@gmail.com>
2025-12-15 16:44:41 -0300

    versao de salvamento da visao de futuro-15-12-16:44

--- config.ts
+++ config.ts
@@ -3,6 +3,6 @@
 // ==========================================================
 
 // URL Fixa do Backend Google Apps Script
-export const API_URL = "https://script.google.com/macros/s/AKfycbwmu0vlJ7iNTAsGpW7t9GSOpdVAAvhg5g6qr8L0BuVGt4Ln9kQapoBwyJfXuTwmtP6uEQ/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbwmu0vlJ7iNTAsGpW7t9GSOpdVAAvhg5g6qr8L0BuVGt4Ln9kQapoBwyJfXuTwmtP6uEQ/exec ";
 
 export const SYSTEM_VERSION = "2.0 Distributed";
