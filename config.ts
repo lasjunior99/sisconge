1ce86d2ea195702ddcd1b041d77178bc827cc185
lasjunior99 <lasjunior99@gmail.com>
2025-12-18 19:46:07 -0300

    Versao-18/12-19:45

--- config.ts
+++ config.ts
@@ -3,6 +3,6 @@
 // ==========================================================
 
 // URL Fixa do Backend Google Apps Script
-export const API_URL = "https://script.google.com/macros/s/AKfycbzsoqlBRElYNLRvVxG-VQb1BeV63eaWDPjPpVCJvknbcMdbhW-3th8AhbI0BnLZWMFdyg/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbyM5oN6fOyxyS8P8wVEBeOaLv1sZrc2NUNz0buT9nAhfEkQp0mXpskL-BAOBJaPJ9huVw/exec";
 
 export const SYSTEM_VERSION = "2.0 Distributed";
